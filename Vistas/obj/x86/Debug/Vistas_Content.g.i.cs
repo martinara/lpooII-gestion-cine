f4b46f5af97b9bb55bbff3b79b67406fd7ad7cc1
Martin Aramayo <mrtn.geek2@gmail.com>
2020-09-21 23:52:53 -0300

    correcion de plantilla de boton e iconos en el login

--- Vistas/obj/x86/Debug/Vistas_Content.g.i.cs
+++ Vistas/obj/x86/Debug/Vistas_Content.g.i.cs
@@ -8,6 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("images/logo-menu.png")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("images/icono-pass.png")]
 
 
